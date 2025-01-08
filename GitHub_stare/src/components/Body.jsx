482c59037d4d785565e5d2fc53fd524f1eafd4f7
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-01-08 14:41:45 +0530

    complete this project

--- GitHub_stare/src/components/Body.jsx
+++ GitHub_stare/src/components/Body.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Card from "./Card";
+import Prev from "./Prev";
 
 const Body = () => {
   const [userName, setUserName] = useState("");
   const [userData, setUserData] = useState([]);
+  const [userAboutData, setUserAboutData] = useState(null);
 
   const fetchApiData = async (userName) => {
     try {
       const response = await axios.get(
         `https://api.github.com/users/${userName}/events`
       );
+      const responseAboutUser = await axios.get(
+        `https://api.github.com/users/${userName}`
+      );
 
+      setUserAboutData(responseAboutUser.data);
       setUserData(response.data);
       console.log(userData);
     } catch (error) {
       console.error("Error fetching in data:", error);
     }
   };
-
+  console.log("this is user about data", userAboutData);
   const submitHandler = (e) => {
     e.preventDefault();
     if (userName) {
@@ -50,13 +56,26 @@ const Body = () => {
         </button>
       </form>
 
+      {userAboutData && (
+        <Prev
+          name={userAboutData.name}
+          imgUrl={userAboutData.avatar_url}
+          loginId={userAboutData.login}
+          loginUrl={userAboutData.url}
+          repoCount={userAboutData.public_repos}
+          gitsCount={userAboutData.public_gists}
+          following={userAboutData.following}
+          follower={userAboutData.followers}
+        />
+      )}
+
       <div className="flex flex-col">
-        {userData.map((instance) => (
+        {userData?.map((instance) => (
           <Card
             type={instance.type}
             created={instance.created_at}
             repoName={instance.repo.name}
-            commitName={instance?.payload?.commits[0]?.message}
+            commitName={instance?.payload?.commits?.[0]?.message} //in my payload if don't have commit so i do it.
             commitUrl={instance?.payload?.head}
           />
         ))}
--- GitHub_stare/src/components/Prev.jsx
+++ GitHub_stare/src/components/Prev.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const Prev = () => {
-  return <div>Prev</div>;
+const Prev = ({
+  name,
+  imgUrl,
+  loginId,
+  loginUrl,
+  repoCount,
+  gitsCount,
+  following,
+  follower,
+}) => {
+  return (
+    <div className="mb-4 flex flex-col items-center gap-5">
+      <img className="rounded-full size-14" src={imgUrl} alt="profile" />
+      <h1>Hello {name}</h1>
+      <h1 className="text-xl underline">
+        Most recent Activity of - <a href={loginUrl}>{loginId}</a>
+      </h1>
+
+      <div className="flex flex-row justify-center space-x-4">
+        <h3 className="border border-gray-500 px-4 py-2 rounded-md text-center shadow-lg">
+          Repos: {repoCount}
+        </h3>
+        <h3 className="border border-gray-500 px-4 py-2 rounded-md text-center shadow-lg">
+          Gists: {gitsCount}
+        </h3>
+        <h3 className="border border-gray-500 px-4 py-2 rounded-md text-center shadow-lg">
+          Following: {following}
+        </h3>
+        <h3 className="border border-gray-500 px-4 py-2 rounded-md text-center shadow-lg">
+          Followers: {follower}
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Prev;
